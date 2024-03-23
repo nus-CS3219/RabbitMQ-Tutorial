249efa929bb67c0b5200582f96d91e3a07f89ea6
Jai2501 <52578354+Jai2501@users.noreply.github.com>
2024-03-23 21:42:12 +0800

    Add CORS

--- Publisher-Subscriber/publisher.js
+++ Publisher-Subscriber/publisher.js
@@ -3,6 +3,20 @@ const amqp = require("amqplib");
 
 const app = express();
 
+// import the dependencies required for cors
+const cors = require("cors");
+
+// allow cross-origin requests to reach the Expres.js server
+// from localhost:3000, which is your frontend domain
+app.options(
+  "*",
+  cors({
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
+  })
+);
+app.use(cors());
+
 app.get("/publish", async (req, res) => {
   const connection = await amqp.connect("amqp://localhost");
   const channel = await connection.createChannel();
